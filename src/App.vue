8e10d564bde11460983edc2c7f13e22a74d7b1fd
Simon <simon@carmona.se>
2021-12-01 16:15:57 +0100

    Loading quiz and getting result

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,181 @@
 <template>
-  <flow-form v-bind:questions="questions" v-bind:progressbar="true" @answer="onAnswer" />
+  <div>
+    <div id="header">Carmona Quiz</div>
+    <flow-form
+      v-bind:questions="questions"
+      v-bind:progressbar="true"
+      @submit="onSubmit"
+      @complete="onComplete"
+      @timer="onTimer"
+      v-bind:language="language"
+      v-bind:standalone="true"
+      v-bind:timer="true"
+      ref="flowform"
+      v-if="formFound"
+    >
+      <template v-slot:complete>
+        <p>
+          <span class="fh2">Bra jobbat!</span>
+          <span v-if="!submitted" class="f-section-text">
+            Du kan gå tillbaka och ändra dina svar eller klicka på nästa för att
+            få ditt resultat :)
+          </span>
+        </p>
+      </template>
+
+      <template v-slot:completeButton>
+        <div class="f-submit" v-if="!submitted">
+          <button
+            class="o-btn-action"
+            ref="button"
+            type="submit"
+            href="#"
+            v-on:click.prevent="onSubmit()"
+            aria-label="Press to submit"
+          >
+            <span>Nästa</span>
+          </button>
+        </div>
+        <p class="text-success" v-if="submitted && time">
+          Din tid: {{ formattedTime }}
+        </p>
+        <p class="text-success" v-if="submitted">
+          Du fick {{ score }} av {{ total }} rätt.
+        </p>
+      </template>
+    </flow-form>
+    <div id="no-form" v-else>
+      <div>No form found!</div>
+    </div>
+  </div>
 </template>
 
 <script>
 import FlowForm, {
   QuestionModel,
   QuestionType,
-  ChoiceOption,
+  ChoiceOption, //eslint-disable-line
+  LanguageModel,
 } from "@ditdot-dev/vue-flow-form";
+
+import quiz from "./forms/quiz.json";
 
 export default {
   name: "App",
   components: {
     FlowForm,
   },
+  mounted() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const formId = urlParams.get("formId");
+    this.getForm(formId);
+  },
   data() {
     return {
-      questions: [
-        // QuestionModel array
-        new QuestionModel({
-          title: "Question",
-          type: QuestionType.MultipleChoice,
-          multiple: false,
-          required: true,
-          options: [
-            new ChoiceOption({
-              label: "Answer 1",
-              correct: true
-            }),
-            new ChoiceOption({
-              label: "Answer 2",
-            }),
-            new ChoiceOption({
-              label: "Answer 3",
-            }),
-            new ChoiceOption({
-              label: "Answer 4",
-            }),
-          ],
-        }),
-      ],
+      submitted: false,
+      completed: false,
+      formFound: false,
+      score: 0,
+      total: 0,
+      time: 0,
+      formattedTime: "",
+      language: new LanguageModel({
+        enterKey: "Enter",
+        shiftKey: "Shift",
+        ok: "OK",
+        continue: "Fortsätt",
+        skip: "Hoppa över",
+        pressEnter: "Tryck :enterKey",
+        multipleChoiceHelpText: "Välj flera svar",
+        multipleChoiceHelpTextSingle: "Välj ett svar",
+        otherPrompt: "Annan",
+        placeholder: "Skriv ditt svar här...",
+        submitText: "Skicka",
+        longTextHelpText: ":shiftKey + :enterKey för att göra ett radbryt.",
+        prev: "Föregående",
+        next: "Nästa",
+        percentCompleted: ":percent% klart",
+        invalidPrompt: "Ogiltig inmatning i formuläret",
+        thankYouText: "Tack!",
+        successText: "Dina svar är inskickde",
+        ariaOk: "Tryck för att fortsätta",
+        ariaRequired: "Obligatoriskt steg",
+        ariaPrev: "Föregående steg",
+        ariaNext: "Nästa steg",
+        ariaSubmitText: "Tryck för att skicka in",
+        ariaMultipleChoice: "Tryck på :letter för att välja",
+        ariaTypeAnswer: "Skriv ditt svar här",
+      }),
+      questions: [],
     };
   },
   methods: {
-    onAnswer(questionAnswered) {
-      console.log(questionAnswered);
-    }
+    onSubmit() {
+      this.$refs.flowform.submitted = true;
+
+      this.submitted = true;
+      this.calculateScore();
+    },
+    onTimer(time, formattedTime) {
+      this.time = formattedTime;
+      this.formattedTime = formattedTime;
+    },
+    calculateScore() {
+      this.questions.forEach((question) => {
+        if (question.type !== QuestionType.SectionBreak) {
+          let answer = question.answer;
+          if (answer === true) {
+            this.score++;
+          }
+        }
+      });
+    },
+    onComplete(completed, questionList) {
+      //eslint-disable-line
+      console.log(questionList);
+      this.completed = completed;
+    },
+    getForm(formId) {
+      let form = quiz[formId];
+      this.total = form.questions.length;
+      if (form) {
+        this.questions.push(
+          new QuestionModel({
+            title: form.title,
+            tagline: this.total + (this.total == 1 ? " fråga" : " frågor"),
+            subtitle: form.subtitle,
+            description: form.description,
+            type: QuestionType.SectionBreak,
+            required: true,
+          })
+        );
+
+        form.questions.forEach((question) => {
+          let title = question.title;
+
+          let options = question.options.map((option) => {
+            return new ChoiceOption({
+              label: option.label,
+              value: option.value
+            });
+          });
+
+          this.questions.push(
+            new QuestionModel({
+              title: title,
+              type: QuestionType.MultipleChoice,
+              multiple: false,
+              required: true,
+              options: options,
+            })
+          );
+        });
+
+        this.formFound = true;
+      } else {
+        this.formFound = false;
+      }
+    },
   },
 };
 </script>
@@ -54,7 +184,29 @@ export default {
 /* Import Vue Flow Form base CSS */
 @import "~@ditdot-dev/vue-flow-form/dist/vue-flow-form.css";
 /* Import one of the Vue Flow Form CSS themes (optional) */
-@import "~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-minimal.css";
+/* @import "~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-minimal.css"; */
 /* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-green.css'; */
-/* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-purple.css'; */
+@import "~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-purple.css";
+
+#header {
+  width: 100%;
+  height: 75px;
+  background: blueviolet;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
+  color: white;
+  font-size: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#no-form {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+}
 </style>
